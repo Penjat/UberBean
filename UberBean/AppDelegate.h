c0efe9d7774add1a1b3f5be6ddb94f6fdd7d5cac
Penjat <theSpencerSymington@Gmail.com>
2019-01-25 15:34:12 -0800

    Initial Commit

---
+++ UberBean/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UberBean
+//
+//  Created by Spencer Symington on 2019-01-25.
+//  Copyright © 2019 Spencer Symington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UberBean/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UberBean
+//
+//  Created by Spencer Symington on 2019-01-25.
+//  Copyright © 2019 Spencer Symington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
