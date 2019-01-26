bd57e8c2e812e385dc32954a31d495d1830226fd
Penjat <theSpencerSymington@Gmail.com>
2019-01-25 16:49:28 -0800

    finds user location

--- UberBean/ViewController.m
+++ UberBean/ViewController.m
@@ -9,7 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet MKMapView *myMapView;
+@property CLLocationManager *locationManager;
 @end
 
 @implementation ViewController
@@ -17,7 +18,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.locationManager = [[CLLocationManager alloc]init];
+    
+    [self.locationManager requestWhenInUseAuthorization];
+    self.locationManager.delegate = self;
+    
 }
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations{
+    
+    CLLocationCoordinate2D location = CLLocationCoordinate2DMake(locations[0].coordinate.latitude,locations[0].coordinate.longitude);
+    
+    MKCoordinateRegion region =  MKCoordinateRegionMake(location, MKCoordinateSpanMake(0.06, 0.06));
+    
+    [_myMapView setRegion:region animated:YES];
+    
+    
+}
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    
+}
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status{
+    [self.locationManager requestLocation];
+}
+
+
+
 
 
 @end
